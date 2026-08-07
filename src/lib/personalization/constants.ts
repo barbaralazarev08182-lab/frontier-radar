a0c0f797ec79d3a42681c5faefc168dc2dbdcfc7
barbaralazarev08182-lab <barbaralazarev08182@gmail.com>
2026-08-07 21:13:36 +0800

    Add personalization visitor constants

---
+++ src/lib/personalization/constants.ts
@@ -0,0 +1,3 @@
+export const VISITOR_STORAGE_KEY = "frontier_radar_visitor_id";
+export const VISITOR_COOKIE = "frontier_radar_visitor_id";
+export const VISITOR_MAX_AGE_SECONDS = 60 * 60 * 24 * 365;
