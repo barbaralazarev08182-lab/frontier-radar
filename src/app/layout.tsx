ef2c915812ced06b9da92b7cf32a9f5ade479b13
Barbaralazarev <barbaralazarev08182@gmail.com>
2026-08-14 16:34:00 +0800

    Gate 15C-A: load canonical system primitives

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { SiteNav } from "@/components/site-nav";
 import "./globals.css";
+import "./frontier-system.css";
 import "./product-grammar.css";
 import "./product-grammar-layer2.css";
 import "./site-nav-transparent-light.css";
