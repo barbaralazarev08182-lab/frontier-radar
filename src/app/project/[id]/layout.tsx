3b23490e4750d5a823c8566251ea96b2431813e6
Barbaralazarev <barbaralazarev08182@gmail.com>
2026-08-14 13:48:27 +0800

    Gate 15B: load editorial drama layer

--- src/app/project/[id]/layout.tsx
+++ src/app/project/[id]/layout.tsx
@@ -3,6 +3,7 @@ import { ProjectReadingController } from "./project-reading-controller";
 import "./project-research-history.css";
 import "./project-lieflat-reading.css";
 import "./project-focus-density.css";
+import "./project-editorial-drama.css";
 
 export default function ProjectIntelligenceLayout({ children }: { children: ReactNode }) {
   return (
